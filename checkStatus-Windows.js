532f49371564a5d0860d69d1a88f39bead554f61
TobyTheTester <tobysinclair86@gmail.com>
2015-04-08 19:30:27 +0100

    Update again

--- checkStatus-Windows.js
+++ checkStatus-Windows.js
@@ -5,10 +5,32 @@
 
 var fs = require('C:/Users/Administrator/AppData/Roaming/npm/node_modules/file-system');
 
-var today = new Date();
-today.getDay();
+function getDateTime() {
 
-fs.appendFile("C:/Program Files (x86)/Jenkins/workspace/RugbyWorldCupTracker/ticketStatus.html", + today + "<br>", function(err) {})
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
+}
+
+fs.appendFile("C:/Program Files (x86)/Jenkins/workspace/RugbyWorldCupTracker/ticketStatus.html", + getDateTime() + "<br>", function(err) {})
 
 describe('Check Status of Tickets', function() {
 
